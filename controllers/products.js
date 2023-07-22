e60b76f5a516619c11ae065dd34ecd40851a81bc
tmgAzit <azit.ghising@gmail.com>
2023-07-22 15:58:16 +0930

    User search query is complete.

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,23 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  // throw new Error('testing async error');
-  res.status(200).json({ msg: `get all static products` });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbhits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
